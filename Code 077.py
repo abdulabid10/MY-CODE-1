8efa7cdc824acb2b34d11eba3bbf40746eb0ebc2
abdulabid10 <ma3641328@gmail.com>
2026-07-21 23:31:49 +0600

    Add prime number checker in Code 077.py

---
+++ Code 077.py
@@ -0,0 +1,15 @@
+n = int(input("Enter a Number: "))
+
+if n < 2:
+    print("Not Prime")
+else:
+    prime = True
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            prime = False
+            break
+
+    if prime:
+        print("Prime Number")
+    else:
+        print("Not Prime")
